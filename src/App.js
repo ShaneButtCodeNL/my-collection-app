5c88666e7decced15a7cda3ac31a799bca689537
ShaneButtCodeNL <shanebuttcode@gmail.com>
2021-04-19 12:18:08 -0230

    Can now connect to server to make rest calls to database

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./app.css";
 import ItemDisplayBox from "./components/ItemDisplayBox";
 import ItemTypeSelectBar from "./components/ItemTypeSelectBar";
 
-//Test Items Delete later
-const testItems = ["All", "Manga", "Anime", "Figures", "Video Games"];
-const testManga = {
-  type: "Manga",
-  imgPath:
-    "https://images-na.ssl-images-amazon.com/images/I/51OiveuWrSL._SX331_BO1,204,203,200_.jpg",
-  details: {
-    volume: 1,
-    name: "Nora",
-    publisher: "ViZ",
-    condition: "Very Good",
-  },
-};
-const testAnime = {
-  type: "Anime",
-  imgPath:
-    "https://upload.wikimedia.org/wikipedia/en/5/5c/Bleach_01_-_The_Substitute.jpg",
-  details: {
-    name: "Bleach Season 1",
-    mediaType: "DVD",
-    publisher: "ViZ",
-    condition: "Like New",
-    limitedEdition: true,
-    genres: ["Shonen,Magic,Battle"],
-  },
-};
-const testVideoGame = {
-  type: "Video Game",
-  imgPath:
-    "https://images-na.ssl-images-amazon.com/images/I/51UTQTqkPaL._AC_SY445_.jpg",
-  details: {
-    name: "Persona 4",
-    platform: "PS2",
-    publisher: "Atlus",
-    condition: "Fair",
-    genres: ["RPG", "Dating Sim", "JRPG"],
-    sealed: false,
-    hasCase: true,
-  },
-};
-const testFigure = {
-  type: "Figure",
-  imgPath:
-    "https://images.goodsmile.info/cgm/images/product/20101104/3012/11913/large/5853bfbe3206658c12ee0eb7a5125e78.jpg",
-  details: {
-    name: "Kousaka Kirino",
-    condition: "Like New",
-    from: "OreImo",
-    type: "Nendroid",
-    sealed: false,
-    ageRestricted: false,
-  },
-};
-
+//Types of Items
 const itemTypes = ["All", "Manga", "Anime", "Figure", "Video Game"];
-
-const testCollection = [testAnime, testManga, testVideoGame, testFigure];
-//End of test items
-
 function App() {
   const [mode, setMode] = useState(1);
   const [selectedItemType, setSelectedItemType] = useState(0);
-  const [itemList, setItemList] = useState(testCollection);
+  const [itemList, setItemList] = useState([]);
   const [filteredItemList, setFilterdItemList] = useState(itemList);
+
   /**
    * Filters list by selected item type
    * @param {Array<CollectionItem>} itemList
@@ -80,6 +25,21 @@ function App() {
       (item) => item.type.toUpperCase() === itemTypes[itemType].toUpperCase()
     );
   };
+
+  useEffect(() => {
+    const apiList = async () => {
+      const res = await axios.get("http://localhost:8000/");
+      if (res.data) {
+        console.log("Fetch Successful");
+        setItemList(
+          res.data.sort((a, b) => {
+            return a.type.localeCompare(b.type);
+          })
+        );
+      }
+    };
+    apiList();
+  }, []);
   useEffect(() => {
     setFilterdItemList(applyFilter(itemList, selectedItemType));
   }, [selectedItemType, itemList]);
@@ -95,7 +55,7 @@ function App() {
         >{`${mode ? "Dark" : "Light"} Mode`}</button>
       </header>
       <ItemTypeSelectBar
-        items={testItems}
+        items={itemTypes}
         mode={mode}
         selectedItemType={selectedItemType}
         setSelectedItemType={setSelectedItemType}
